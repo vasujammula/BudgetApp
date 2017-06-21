98182f6d6418c92b0de86ca599979e74f9ff55d8
vasujammula <vasu1824@gmail.com>
2017-06-21 08:34:15 +0530

    Action on press add transaction

---
+++ python_sqlite.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import sqlite3
+import os
+import sys
+db_location="transactions.db"
+class data_handler():
+	def __init__(self):
+		print "INFO:Checking and initializing database"
+		if(os.path.exists(db_location)):
+			pass
+			#Schema for table #c.execute('''CREATE TABLE transactions(amount text, date text, type text)''')
+		else:
+			print "ERROR:Database Not Present "
+			sys.exit(1)
+	def add_trans(self,amount,date,ttype):
+		conn_h=sqlite3.connect(db_location)
+		if(conn_h):
+			c_h=conn_h.cursor()	
+			if c_h:
+				c_h.execute("INSERT INTO transactions VALUES ('%s','%s','%s')"%(amount,date,ttype))
+				conn_h.commit()
+				conn_h.close()
+			else:
+				print "ERROR:ERROR WHILE INSERTING"		
+		else:
+			print "ERROR:ERORR WHILE OPENEING DATABASE"
+		
+
+	def view_trans(self):
+		conn_h=sqlite3.connect(db_location)
+                if(conn_h):
+                        c_h=conn_h.cursor()
+                        if c_h:
+				c_h.execute("SELECT * FROM transactions ")
+                                rows=c_h.fetchall()
+                		for record in rows:
+                        		print record
+				conn_h.close
+                        else:
+                                print "ERROR:ERROR WHILE INSERTING"
+                else:
+                        print "ERROR:ERORR WHILE OPENEING DATABASE"
+
+
+
+if __name__=="__main__":
+	d=data_handler()
+	#d.add_trans("3000","may17","D")
+	d.view_trans()
